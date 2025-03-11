19d4aba8132b17e3bc1069f812175748c118be02
TLouf <loufthomas@gmail.com>
2025-03-11 12:06:55 +0100

    clarify `channels` fields with reorder

--- chan-querier/channels.sql
+++ chan-querier/channels.sql
@@ -5,20 +5,23 @@ CREATE TABLE
         parent_channel_id BIGINT NOT NULL,
         access_hash BIGINT NOT NULL,
         username VARCHAR(255),
-        data_owner VARCHAR(255),
-        language_code VARCHAR(20),
-        search_id VARCHAR(255),
-        keyword_id VARCHAR(255),
-        keyword VARCHAR(255),
-        query_id VARCHAR(255),
-        query_date TIMESTAMP,
-        nr_participants INT DEFAULT 0,
-        distance_from_core INT DEFAULT 0,
         channel_last_queried_at TIMESTAMP WITH TIME ZONE,
         messages_last_queried_at TIMESTAMP WITH TIME ZONE,
         last_queried_message_id INT DEFAULT 0,
+        -- priority data
+        language_code VARCHAR(20),
+        nr_participants INT DEFAULT 0,
+        distance_from_core INT DEFAULT 0,
         message_count INT DEFAULT 0,
         collection_priority NUMERIC(10, 9)
+        -- search info
+        search_id VARCHAR(255),
+        keyword_id VARCHAR(255),
+        keyword VARCHAR(255),
+        -- query info
+        data_owner VARCHAR(255),
+        query_id VARCHAR(255),
+        query_date TIMESTAMP,
     );
 
 CREATE INDEX telegram_channels_id_index ON telegram.channels (id);
