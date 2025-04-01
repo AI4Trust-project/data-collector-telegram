fa979cd6403596b6f3db3c4711b28fc71888a3e4
TLouf <loufthomas@gmail.com>
2025-04-01 13:57:21 +0200

    fix: missing return in json default fun

--- chan-querier/chan_querier.py
+++ chan-querier/chan_querier.py
@@ -71,7 +71,7 @@ def _iceberg_json_default(value):
     if isinstance(value, datetime.datetime):
         return value.strftime("%Y-%m-%dT%H:%M:%SZ")
     elif isinstance(value, set):
-        value = list(value)
+        return list(value)
     else:
         return repr(value)
 
